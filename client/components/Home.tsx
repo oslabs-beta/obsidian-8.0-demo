12fbfd74d6091f8e0f24df067e93fbddbf5ae258
Kevin <kevin.06.huang@gmail.com>
2023-02-04 02:12:26 -0800

    Minor refactor

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -2,7 +2,7 @@ import { React, useObsidian, BrowserCache } from "../../deps/deps.client.ts";
 
 const Home = () => {
   const { query, cache, setCache } = useObsidian();
-  const [movies, setMovies] = React.useState('');
+  const [people, setPeople] = React.useState('');
 
   const queryStr = `query {
       allPeople {
@@ -18,10 +18,10 @@ const Home = () => {
       <button
       onClick={() => {
         query(queryStr)
-        .then(resp => setMovies(resp.data))
+        .then(resp => setPeople(resp.data))
         .then(resp => setCache(new BrowserCache(cache.storage)))
       }}
-    >Get Movies</button>
+    >Get People</button>
     </div>
   );
 }
