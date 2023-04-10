df9c0fd5f172d79bbf25dbfc349b5937a6336cc8
davidtoyoukim <david.toyou.kim@gmail.com>
2023-04-10 10:59:30 -0700

    removed submodule config

--- obsidian/clientMod.ts
+++
@@ -1,9 +0,0 @@
-import {
-  ObsidianWrapper,
-  useObsidian,
-} from './ObsidianWrapper/ObsidianWrapper.jsx';
-import BrowserCache from './src/Browser/CacheClassBrowser.js';
-
-import LFUCache from './src/Browser/lfuBrowserCache.js';
-
-export { BrowserCache, ObsidianWrapper, useObsidian, LFUCache };
--- obsidian/mod.ts
+++
@@ -1,4 +0,0 @@
-import { ObsidianRouter } from './src/Obsidian.ts';
-import gql from 'https://deno.land/x/oak_graphql@0.6.2/graphql-tag/index.ts';
-
-export { ObsidianRouter, gql };
